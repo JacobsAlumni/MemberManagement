4a4361460cd1fb5cbc746345aca7de96ad74b94e
Siddharth Shukla <siddharth.shukla@uber.com>
2017-12-03 11:21:38 +0100

    add exception cases for common stripe errors

--- registry/views/setup.py
+++ registry/views/setup.py
@@ -146,7 +146,35 @@ class SubscribeView(FormView):
         try:
             customer = stripe.Customer.create(**customer_data)
             subscription = customer.subscriptions.create(plan=subscription_plans[tier].stripe_id)
-        except Exception:
+        except stripe.error.CardError as e:
+            # Since it's a decline, stripe.error.CardError will be caught
+            body = e.json_body
+            err = body.get('error', {})
+
+            print "Status is: %s" % e.http_status
+            print "Type is: %s" % err.get('type')
+            print "Code is: %s" % err.get('code')
+            # param is '' in this case
+            print "Param is: %s" % err.get('param')
+            print "Message is: %s" % err.get('message')
+        except stripe.error.RateLimitError as e:
+            # Too many requests made to the API too quickly
+            pass
+        except stripe.error.InvalidRequestError as e:
+            # Invalid parameters were supplied to Stripe's API
+            pass
+        except stripe.error.AuthenticationError as e:
+            # Authentication with Stripe's API failed
+            # (maybe you changed API keys recently)
+            pass
+        except stripe.error.APIConnectionError as e:
+            # Network communication with Stripe failed
+            pass
+        except stripe.error.StripeError as e:
+            # Display a very generic error to the user, and maybe send
+            # yourself an email
+            pass
+        except Exception as e:
             form.add_error(None, 'Something went wrong trying to process your payment. Please try again. ')
             return self.form_invalid(form)
 
