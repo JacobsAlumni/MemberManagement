f664bf95d3b4dc3f641069eb8c4b69c1c039b497
Tom Wiesing <tkw01536@gmail.com>
2017-12-03 17:41:03 +0100

    Portal page

--- registry/templatetags/registry_display.py
+++ registry/templatetags/registry_display.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter('get_choice_field')
 def get_choice_field(instance, name):
     choices = dict(type(instance)._meta.get_field(name).choices)
@@ -10,3 +11,8 @@ def get_choice_field(instance, name):
         return [choices[v] for v in value]
     else:
         return choices[value]
+
+
+@register.filter('print_boolean')
+def print_boolean(value):
+    return 'Yes' if value else 'No'
