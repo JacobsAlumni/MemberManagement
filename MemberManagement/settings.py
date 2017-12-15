93efb7b8031b6545a9fb412c5c9cc71ae6a2710e
Tom Wiesing <tkw01536@gmail.com>
2017-12-15 02:06:46 +0100

    Add a warning when in devel mode

--- MemberManagement/settings.py
+++ MemberManagement/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'registry.context_processors.devel_warning',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
---
+++ registry/context_processors.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+
+NEEDS_DEVEL_WARNING = getattr(settings, 'ENABLE_DEVEL_WARNING',
+                              False)
+
+
+def devel_warning(request):
+    return {'show_devel_warning': NEEDS_DEVEL_WARNING}
