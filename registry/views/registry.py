e20870ae494c52e1c85ababf0893d53cad471a6e
Tom Wiesing <tkw01536@gmail.com>
2018-11-07 16:39:09 +0100

    Catch ObjectDoesNotExist on portal homepage
    This commit catches ObjectDoesNotExist when a user without an alumni
    attempts to view the home page.

--- registry/views/registry.py
+++ registry/views/registry.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 from registry.decorators import require_setup_completed
 from registry.models import Announcement
@@ -9,9 +11,12 @@ from registry.models import Announcement
 def home(request):
     """ Renders either the home page or the portal. """
 
-    # if the user is signed in, redirect to the main portal
-    if request.user.is_authenticated() and request.user.alumni:
-        return redirect(reverse('portal'))
+    try:
+        # if the user is signed in, redirect to the main portal
+        if request.user.is_authenticated() and request.user.alumni:
+            return redirect(reverse('portal'))
+    except ObjectDoesNotExist:
+        return HttpResponse('Unauthorized (no alumni for user)', status=401)
 
     # else render the home page
     return render(request, 'registry/index.html')
