aa3d5c09383a288da06d2a5a96e073a35d8d2875
Tom Wiesing <tkw01536@gmail.com>
2019-01-15 23:45:51 +0100

    Alumni Tiers: Change order and description

--- alumni/fields.py
+++ alumni/fields.py
@@ -770,10 +770,10 @@ class TierField(models.CharField):
     TIER_CHOICES = (
         (CONTRIBUTOR,
          "Contributor (Standard package for graduation before 2016): 39€ p.a."),
+        (STARTER,
+         "Starter (If graduated less than 2 years ago or not ready to financially contribute): free"),
         (PATRON,
          "Patron (Premium package for those who want to contribute more): 249€ p.a. "),
-        (STARTER,
-         "Starter (For graduation after 2016 or unable to financially contribute): 1 symbolic cent p.a."),
     )
 
     def __init__(self, **kwargs):
