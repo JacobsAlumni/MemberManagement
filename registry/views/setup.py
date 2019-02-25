f9bf0ec7dd0a3b3c622102bef6009ef88dd15b3e
Tom Wiesing <tkw01536@gmail.com>
2019-02-25 11:10:34 +0100

    Change Setup Button Text
    This commit changes the initial text on the application submission page
    from "Join the Alumni Association" to "Continue Application".

--- registry/views/setup.py
+++ registry/views/setup.py
@@ -62,7 +62,7 @@ def register(request):
         'form': form,
         'title': 'Register',
         'subtitle': 'Enter your General Information - just the basics',
-        'next_text': 'Join the Alumni Association'
+        'next_text': 'Continue Application'
     })
 
 
