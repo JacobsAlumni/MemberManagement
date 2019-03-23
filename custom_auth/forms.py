f3eba58114c5a81f15d84039531364f7028617b8
Tom Wiesing <tkw01536@gmail.com>
2019-03-23 12:43:36 +0100

    login: clarify backup email address field

--- custom_auth/forms.py
+++ custom_auth/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
 class EmailForm(forms.Form):
-    email = forms.EmailField(label='Your backup e-mail address',
+    email = forms.EmailField(
+        label='Your backup e-mail address',
+        help_text='The private email you first used to register on the membership portal. ',
         max_length=100)
