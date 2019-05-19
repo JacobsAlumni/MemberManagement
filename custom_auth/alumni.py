0b900c2ce5d2d83dd0d5f9fb5553f41485cf5855
Tom Wiesing <tkw01536@gmail.com>
2019-05-19 22:49:43 +0200

    Update E-Mail template (untested)

--- custom_auth/alumni.py
+++ custom_auth/alumni.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from .mailutils import send_email
+import datetime
 
 from alumni import fields
 
@@ -8,19 +9,21 @@ class AlumniEmailMixin:
     def send_welcome_email(self, password=None, back=False):
         """ Sends a user a Welcome (or welcome back) email """
 
-        # Extract all the fields from the alumni
-        email = self.email
-        gsuite = self.approval.gsuite
-        name = self.fullName
-        tier = {
-            fields.TierField.PATRON: 'Patron',
-            fields.TierField.CONTRIBUTOR: 'Contributor',
-            fields.TierField.STARTER: 'Starter'
-        }[self.membership.tier]
+        context = {
+            'FullName': self.fullName,
+            'Tier': {
+                fields.TierField.PATRON: 'Patron',
+                fields.TierField.CONTRIBUTOR: 'Contributor',
+                fields.TierField.STARTER: 'Starter'
+            }[self.membership.tier],
+            'CurrentDate': datetime.date.today().strftime('%d %B %Y'),
+            'Email': self.email,
+            'Password': password,
+        }
 
         # set destination and instantiate email templates
         destination = [email, gsuite] + settings.GSUITE_EMAIL_ALL
-        if back or password is None:
-            return send_email(destination, settings.GSUITE_EMAIL_WELCOMEBACK_SUBJECT, 'emails/welcomeback_email.html', name=name, tier=tier, gsuite=gsuite, password=password)
+        if back:
+            return send_email(destination, settings.GSUITE_EMAIL_WELCOMEBACK_SUBJECT, 'emails/approval/existing.html', **context)
         else:
-            return send_email(destination, settings.GSUITE_EMAIL_WELCOME_SUBJECT, 'emails/welcome_email.html', name=name, tier=tier, gsuite=gsuite, password=password)
+            return send_email(destination, settings.GSUITE_EMAIL_WELCOME_SUBJECT, 'emails/approval/new.html', **context)
