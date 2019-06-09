38d3e99e8a747a4f3740aafed371cad3e111df6b
Tom Wiesing <tkw01536@gmail.com>
2019-06-09 11:41:09 +0200

    Add DateField Polyfill
    This commit adds a polyfill for date input fields

--- django_forms_uikit/widgets.py
+++ django_forms_uikit/widgets.py
@@ -5,5 +5,11 @@ from django.forms.widgets import DateInput
 
 
 class DatePickerInput(DateInput):
+    input_type = 'date'
     format_key = 'DATE_INPUT_FORMATS'
     template_name = 'uikit/field_date.html'
+
+    def __init__(self, attrs=None):
+        if attrs is None:
+            attrs = {}
+        super().__init__(attrs=attrs, format="%Y-%m-%d")
