ffc29eb7b3a65c56c74a9014b253e007663cbc78
Tom Wiesing <tkw01536@gmail.com>
2019-09-18 15:06:07 +0200

    Don't show empty inlines
    This commit prevents editable inlines from being shown unless clicked on
    explicitly in Django Admin.

--- alumni/admin/inlines.py
+++ alumni/admin/inlines.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from ..models import Alumni, Address, JobInformation, SocialMedia, \
+from ..models import Address, JobInformation, SocialMedia, \
     JacobsData, Approval, Skills, SetupCompleted
 
 from atlas.admin import AtlasSettingsInline
@@ -8,30 +8,37 @@ from payments.admin import MembershipInformationInline, SubscriptionInformationI
 
 
 class JacobsDataInline(admin.StackedInline):
+    extra = 0
     model = JacobsData
 
 
 class SocialMediaInline(admin.StackedInline):
+    extra = 0
     model = SocialMedia
 
 
 class AddressInline(admin.StackedInline):
+    extra = 0
     model = Address
 
 
 class JobInformationInline(admin.StackedInline):
+    extra = 0
     model = JobInformation
 
 
 class ApprovalInline(admin.StackedInline):
+    extra = 0
     model = Approval
 
 
 class SkillsInline(admin.StackedInline):
+    extra = 0
     model = Skills
 
 
 class SetupCompletedInline(admin.StackedInline):
+    extra = 0
     model = SetupCompleted
 
 
