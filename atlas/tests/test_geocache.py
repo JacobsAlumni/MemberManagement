c0042f87ff51cca6e7463ad66624b830c8078c2d
Tom Wiesing <tkw01536@gmail.com>
2020-02-11 16:39:05 +0100

    Rework 'geocache' manage.py command
    This commit cleans up the geocache command. In particular it now:
    - takes an optional url argument
    - show a better progress bar
    - is optionally tested

---
+++ atlas/tests/test_geocache.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from atlas.models import GeoLocation
+from django.core import management
+import os
+
+class GeoCacheTest(TestCase):
+    def test_download(self):
+        if os.environ.get('ENABLE_GEOCACHE_TEST', "0") != "1":
+            self.skipTest('Skipping, set ENABLE_GEOCACHE_TEST=1 to enable')
+
+        management.call_command(
+            'geocache', url="https://github.com/JacobsAlumni/geonames.org-mirror/releases/download/v2020.01/allCountries.zip")
+        self.assertEqual(GeoLocation.objects.count(), 852799,
+                         'Correct number of objects received')
