bef21aac298c5e60e52b3f50cebb790c51b789eb
Tom Wiesing <tkw01536@gmail.com>
2020-03-28 19:47:27 +0100

    Rename {% block scripts %} to {% block extrascripts %}

--- assets/src/base/index.ts
+++ assets/src/base/index.ts
@@ -1,3 +1,4 @@
 require('./libs/jquery.js'); // jquery
 require('./libs/uikit.js'); // uikit
+require('./libs/cookielaw.js'); // cookielaw
 import 'nodep-date-input-polyfill'; // nodep-date-input-polyfill
