cc3996d672fff7ee85b6507da774a64e515c44f3
Tom Wiesing <tkw01536@gmail.com>
2020-04-19 11:36:41 +0200

    custom_auth: URL consistency

--- custom_auth/urls.py
+++ custom_auth/urls.py
@@ -9,7 +9,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     # Login / Logout
     url(r'^login/$', views.ClientIdLoginView.as_view(template_name='auth/login.html'), {}, name='login'),
-    url('^logout/$', LogoutView.as_view(), name='logout'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
 
     url(r'^token/sent/', generic_views.TemplateView.as_view(
         template_name='auth/token_sent.html'), name='token_sent'),
