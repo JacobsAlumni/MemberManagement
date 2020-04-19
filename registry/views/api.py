7d5f2070af3f663c9549ad10305e78f8a43accbf
Tom Wiesing <tkw01536@gmail.com>
2020-04-19 11:25:39 +0200

    Add FormValidationView that validates a form without executing it

--- registry/views/api.py
+++ registry/views/api.py
@@ -1,7 +1,44 @@
-from rest_framework import generics
-from django.contrib.auth import get_user_model
-from .. import serializers
+from __future__ import annotations
+from django.utils.decorators import method_decorator
+import json
 
-class CreateUserView(generics.CreateAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = serializers.CreateUserSerializer
+from django.views import View
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_protect
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from django.forms import Form
+    from django.http import HttpRequest, HttpResponse
+
+from ..forms import RegistrationForm
+
+
+@method_decorator(csrf_protect, name='dispatch')
+class FormValidationView(View):
+    """ A view that can be posted with form data to validate it """
+
+    # the form this view is validating
+    form: Form = None
+
+    @classmethod
+    def instantiate_form(cls, request: HttpRequest) -> Form:
+        """ Used to instantiate the form given a request"""
+        return cls.form(request.POST)
+
+    def post(self, request: HttpRequest) -> HttpResponse:
+        """ Validates form data via POST """
+
+        # instatiate the form
+        form = self.__class__.instantiate_form(request)
+        
+        # check if the form is valid
+        valid = form.is_valid()
+        errors = json.loads(form.errors.as_json())
+
+        # and return the response
+        return JsonResponse(data={"valid": valid, "errors": errors})
+
+
+class RegistrationValidationView(FormValidationView):
+    form = RegistrationForm
