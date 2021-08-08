2c2802992f5dad6a0052a3502cd55ce90f65dee7
Tom Wiesing <tkw01536@gmail.com>
2021-08-08 15:14:40 -0400

    Add labels to top of alumni/change_form.html

---
+++ alumni/templatetags/changelist_link.py
@@ -0,0 +1,26 @@
+from urllib.parse import urlencode
+from django.template import Library
+from django.urls import reverse
+from urllib.parse import parse_qsl
+
+register = Library()
+
+
+@register.simple_tag(takes_context=True)
+def changelist_link(context, filter: str, value: str) -> str:
+    opts = context.get('opts')
+    preserved_filters = context.get('preserved_filters')
+
+    query = dict()
+
+    # extract query from the url!
+    if preserved_filters:
+        query = dict(parse_qsl(preserved_filters))
+        if '_changelist_filters' in query:
+            query = dict(parse_qsl(query['_changelist_filters']))
+
+    # update the query
+    query[filter] = value
+
+    # and make a changelist link!
+    return reverse('admin:{}_{}_changelist'.format(opts.app_label, opts.model_name)) + '?' + urlencode(query)
