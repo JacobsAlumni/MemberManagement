7597619bb30e51667d3813f48e2d65826dbf3224
Leonhard Kuboschek <leo@jacobs-alumni.de>
2022-05-06 11:45:22 +0200

    Bump Django and related package version

--- donation_receipts/models.py
+++ donation_receipts/models.py
@@ -4,7 +4,7 @@ from os import path
 
 from django.db import models
 from django.dispatch import receiver
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.db.models import signals
 from django.core.files.base import ContentFile
 from django.contrib.auth import get_user_model
