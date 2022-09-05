212f90956679e4720550ee0ad7ffc3aa81b5575f
Leonhard Kuboschek <leo@legartis.ai>
2022-09-05 22:51:33 +0200

    Nicer donation page

--- assets/src/donate/form/index.ts
+++ assets/src/donate/form/index.ts
@@ -2,23 +2,42 @@ import './index.css';
 
 const total_donations = document.getElementById('total-donations') as HTMLDivElement;
 const new_donations = document.getElementById('current-donations') as HTMLDivElement;
+const live_display = document.getElementById('live-display') as HTMLDivElement
+
+const thankYous = [
+    "Very generous.",
+    "Thank You!",
+    "Much appreciated!"
+]
 
 
-function formatAmount(amount: number) {
-    const inEuros = amount / 100;
-
-    return `${inEuros.toFixed(2)} €`
+function numberWithDots(x: string) {
+    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ".");
 }
 
+function formatAmount(amount: number) {
+    const inEuros = (amount / 100).toLocaleString('de')
+
+
+    return `${inEuros} €`
+}
 
 const prot = window.location.protocol.replace("http", "ws")
 
 const socket = new WebSocket(prot + '//' + window.location.host + window.location.pathname);
+
+socket.onclose = function () {
+    live_display.classList.add("uk-hidden")
+}
+
 socket.onmessage = function (event) {
+    live_display.classList.remove("uk-hidden")
+
     const {
         amounts: {
             total,
-            current
+            current,
+            for_target
         }
     } = JSON.parse(event.data)
 
@@ -29,7 +48,13 @@ socket.onmessage = function (event) {
 
     if(current){
         const li = document.createElement('li')
-        li.innerHTML = `<li>${formatAmount(current)}</li>`
+        li.innerHTML = formatAmount(current)
+
+        if(for_target) {
+            li.innerHTML += ` for ${for_target}`
+        }
+
+        li.innerHTML += `. ${thankYous[Math.floor(Math.random() * thankYous.length)]}`
 
         new_donations.appendChild(li)
     }
--- donations/signals.py
+++ donations/signals.py
@@ -2,6 +2,7 @@ from django.dispatch import receiver
 from django.db.models import signals
 from channels import layers
 from asgiref import sync
+from donations.models import Donation
 
 from donations.utils import get_total_payments
 
@@ -11,6 +12,15 @@ def _maybe_complete_donation(sender, instance, created, **kwargs):
     if instance.data["status"] == "succeeded":
         total_amount = get_total_payments()
         new_amount = instance.data["amount"]
+        for_target = None
+
+        # Find donation for this PaymentIntent
+        try:
+            d = Donation.objects.get(payment_id=instance.data["id"])
+            if d.target:
+                for_target = str(d.target)
+        except Donation.DoesNotExist:
+            pass
 
         layer = layers.get_channel_layer()
-        sync.async_to_sync(layer.group_send)("donation_updates", {"type": "donations.total", "amounts": {"total": total_amount, "current": new_amount}})
+        sync.async_to_sync(layer.group_send)("donation_updates", {"type": "donations.total", "amounts": {"total": total_amount, "current": new_amount, "for_target": for_target}})
